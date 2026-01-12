dac80ecf6cd6374f78fb87b1b9eff16e06534dd9
Fax79 <fabio.terrasi@gmail.com>
2026-01-12 11:40:02 +0100

    uffa

--- components/TravelWidget.tsx
+++ components/TravelWidget.tsx
@@ -7,13 +7,13 @@ type Props = {
 export default function TravelWidget({ url }: Props) {
   return (
     <div className="w-full my-12 flex justify-center">
-      <div className="w-full max-w-[800px] h-[500px] bg-gray-50 rounded-2xl overflow-hidden shadow-inner border border-gray-100">
+      <div className="w-full max-w-[800px] min-h-[500px] bg-gray-50 rounded-2xl overflow-hidden shadow-inner border border-gray-100">
         <iframe 
           src={url} 
           title="Widget Viaggi"
           width="100%" 
-          height="100%" 
-          style={{ border: 'none' }}
+          height="500px" 
+          style={{ border: 'none', display: 'block' }}
         />
       </div>
     </div>
